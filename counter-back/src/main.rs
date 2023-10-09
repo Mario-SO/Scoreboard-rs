ddd467dc35af2ccbc4776a4822ad32297b104331
Mario S.O <gh_mario.proclaim@slmail.me>
2023-10-09 05:04:32 -0400

    🔥: root handler example

--- counter-back/src/main.rs
+++ counter-back/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::{get, Router};
+use sqlx::error::BoxDynError;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let PORT = 3000;
+    let ADDR = format!("0.0.0.0:{}", PORT);
+
+    let app = Router::new().route("/", get(root));
+
+    axum::Server::bind(&ADDR.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+async fn root() -> &'static str {
+    "hello bruh"
 }
